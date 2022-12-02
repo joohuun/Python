ba0426b8c486670b2ff69b664b00b3eac0831fe3
juhoon <k00005781@gmail.com>
2022-12-02 17:11:14 +0900

    todo: article mutation

--- GraphQL/sns/project/graphql/article/mutations.py
+++ GraphQL/sns/project/graphql/article/mutations.py
@@ -2,3 +2,28 @@ import graphene
 from account.models import User
 from article.models import Article
 from graphql import GraphQLError
+from article.types import ArticleType, CommentType, LikeType
+from graphql_jwt.decorators import login_required
+
+
+class CreateArticle(graphene.Mutation):
+    class Arguments:
+        title = graphene.String()
+        body = graphene.String()
+
+    article = graphene.Field(ArticleType)
+
+    @classmethod
+    @login_required
+    def mutate(cls, root, info, title, body):
+        if info.context.user:
+            print(info.context.user.id)
+        article = Article(title=title, body=body)
+        user = User.objects.get(id=info.context.user.id)
+        is_verified = user.status.verified
+        if not is_verified:
+            raise GraphQLError("인증되지 않은 사용자입니다")
+
+        article.user = user
+        article.save()
+        return CreateArticle(article=article)
